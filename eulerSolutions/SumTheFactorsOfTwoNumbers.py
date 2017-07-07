e4baccf16df905c6ef1b32c0fc3eaa241d386d15
malachi <malachi@a>
2017-07-06 23:13:31 -0400

    Change-Id: Ib0d162253841ef2eacf674c66910bef46c8835c4

--- eulerSolutions/SumTheFactorsOfTwoNumbers.py
+++ eulerSolutions/SumTheFactorsOfTwoNumbers.py
@@ -2,7 +2,7 @@
 Created on Jul 6, 2017
 
 @author: Malachi Woodlee
-@note: Project Euler
+@note: Project Euler No.1
 
 
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
