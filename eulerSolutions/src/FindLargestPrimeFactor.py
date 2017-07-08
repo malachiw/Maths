4f69f8dfc8f0f8d3f94327878cc9ed6de98c177e
malachi <malachi@a>
2017-07-08 08:29:05 -0400

    Added def prime_factors method to problem 3

--- eulerSolutions/src/FindLargestPrimeFactor.py
+++ eulerSolutions/src/FindLargestPrimeFactor.py
@@ -7,3 +7,15 @@ What is the largest prime factor of the number 600851475143?
 '''
 NUMBER_TO_FACTOR = 600851475143
 
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+return factors 
