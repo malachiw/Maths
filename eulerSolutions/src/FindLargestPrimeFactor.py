31559f65affabe06ec087d3f23aae2efa6eac5c6
malachi <malachi@a>
2017-07-08 08:42:42 -0400

    added module helpful then changed its name to maths_functions.

--- eulerSolutions/src/FindLargestPrimeFactor.py
+++ eulerSolutions/src/FindLargestPrimeFactor.py
@@ -5,7 +5,7 @@ Created on Jul 7, 2017
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 '''
-import helpful.prime_factors
+import maths_functions.prime_factors
 
 NUMBER_TO_FACTOR = 600851475143
 
